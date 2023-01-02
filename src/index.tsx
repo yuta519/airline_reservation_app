28fc96e846e121fd34b5357ec7cd0576134ff5dd
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-01-03 00:04:50 +0900

    add unnecessary imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,17 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
   <Router>
     <Routes>
       <Route path="/" element={<App />} />
       <Route></Route>
     </Routes>
   </Router>
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
