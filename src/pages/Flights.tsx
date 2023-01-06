9977a2ebdefacbe208d8cb1eac990228ea192967
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-01-07 07:44:13 +0900

    use flight components

--- src/pages/Flights.tsx
+++ src/pages/Flights.tsx
@@ -1,5 +1,55 @@
+import { useState } from "react";
+import { FlightSearchForm } from "../components/FlightSearchForm";
+import { FlightSearchResult } from "../components/FlightSearchResult";
+import { FetchFlightOffers } from "../infra/amadeus";
+import { FlightOffer } from "../types/amadeus";
+
+interface State {
+  countOfResults: number;
+  flightOffers: FlightOffer[];
+}
+
 const Flights = () => {
-  return <>hoge</>;
+  const [state, update] = useState<State>({
+    countOfResults: 0,
+    flightOffers: [],
+  });
+
+  const handleSubmitFlightSearch = async (params: {
+    departureCode: string;
+    destinationCode: string;
+    departureDate: string;
+    numberOfadults: number;
+    currencyCode: string;
+  }) => {
+    const result = await FetchFlightOffers(
+      params.departureCode,
+      params.destinationCode,
+      params.departureDate,
+      params.numberOfadults,
+      params.currencyCode
+    );
+    update({
+      countOfResults: result.countOfResults,
+      flightOffers: result.flightOffers,
+    });
+  };
+
+  return (
+    <>
+      <img
+        className="rounded-lg shadow-xl"
+        src="https://content.skyscnr.com/m/cce15d2addefd0d/original/Japan_Homepage_Hero.png?resize=2000px:1400px&quality=60"
+        alt="Woman workcationing on the beach"
+      />
+      <FlightSearchForm onSubmit={handleSubmitFlightSearch} />
+      {state.countOfResults
+        ? state.flightOffers.map((flightOffer) => (
+            <FlightSearchResult flightOffer={flightOffer} />
+          ))
+        : null}
+    </>
+  );
 };
 
 export default Flights;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,10 @@
-import { Header } from "../components/Header";
-
 const Home = () => {
   return (
     <>
-      <Header />
-      <section className="bg-white dark:bg-gray-900">
+      {/* <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
           <a
-            href="#"
+            href="/home"
             className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             role="alert"
           >
@@ -39,7 +36,7 @@ const Home = () => {
           </p>
           <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
             <a
-              href="#"
+              href="/home"
               className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
             >
               Learn more
@@ -178,6 +175,14 @@ const Home = () => {
               </a>
             </div>
           </div>
+        </div>
+      </section> */}
+
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            We travel in the world
+          </h1>
         </div>
       </section>
     </>
