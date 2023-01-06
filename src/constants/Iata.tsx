c490b70fe92f31e1b3a30eed613c7080293adefd
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-01-07 04:49:12 +0900

    Create constants for IATA code

---
+++ src/constants/Iata.tsx
@@ -0,0 +1,25 @@
+export const AirportIataCodes: { [key: string]: string } = {
+  // CANADA
+  YYC: "Calgary Airport, Canada",
+  YEG: "Edmonton Airport, Canada",
+  YFC: "Fredericton Airport, Canada",
+  YQX: "Gander Airport, Canada",
+  YHZ: "Halifax Stanfield Airport, Canada",
+  YQM: "Greater Moncton Roméo LeBlanc Airport, Canada",
+  YUL: "Montréal–Trudeau Airport, Canada",
+  YOW: "Ottawa Macdonald–Cartier Airport, Canada",
+  YQB: "Québec/Jean Lesage Airport, Canada",
+  YYT: "St. John's Airport, Canada",
+  YYZ: "Toronto Pearson Airport, Canada",
+  YVR: "Vancouver Airport, Canada",
+  YWG: "Winnipeg Airport, Canada",
+  // JAPAN
+  NRT: "Narita Airport, Japan",
+  HND: "Haneda Airport, Japan",
+};
+
+export const CurrencyIataCodes: { [key: string]: string } = {
+  CAD: "Canadian Dollar",
+  USD: "US Dollar",
+  JPY: "Japan Yen",
+};
