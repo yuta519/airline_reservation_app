90afaf34fda445b2ca058c5de6262774161ba7ca
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-01-09 22:33:36 +0900

    make amadesu authorize function private

--- src/infra/amadeus.tsx
+++ src/infra/amadeus.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { FlightOffer } from "../types/amadeus";
 
-export const authorize = async () => {
+const authorize = async () => {
   const response = await axios.post(
     "https://test.api.amadeus.com/v1/security/oauth2/token",
     `grant_type=client_credentials&client_id=${process.env.REACT_APP_AMADEUS_KEY}&client_secret=${process.env.REACT_APP_AMADEUS_SECRET}`,
